5ccbff30ae380938fdf5909e260dabb52668576c
Octavio Thomsen <otithomsen99@gmail.com>
2026-09-25 18:45:40 -0300

    Implemento politica para evitar cors y permitir un futuro frontend

--- ApiPractice/Program.cs
+++ ApiPractice/Program.cs
@@ -4,8 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
